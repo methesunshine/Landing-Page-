85285fac61285e9d3aafa6d4363ff625ba9044d8
Anjali Kumar <anjalikumar666@gmail.com>
2025-05-07 17:16:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function scrollToSection() {
+  const featuresSection = document.getElementById("features");
+  if (featuresSection) {
+    featuresSection.scrollIntoView({ behavior: "smooth" });
+  }
+}
+  
